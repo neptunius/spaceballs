aa6345e9f5b8e54803bd68fc47ad95e483881bed
Alan Davis <neptunius@gmail.com>
2019-12-02 02:22:22 -0800

    Shapes now support 15 different geometries

--- js/shape.js
+++ js/shape.js
@@ -5,6 +5,7 @@ class Shape {
     this.size = args.size || randomInt(10, 50);
     // Initialize geometry and mesh
     this.initGeometry(args);
+    this.initMesh(args);
     // Translational velocity
     this.velocity = randomVector3(-5, 5);
     // Rotational velocity
@@ -14,10 +15,74 @@ class Shape {
     this.recolor();
   };
 
+  static randomShape() {
+    const shapes = [ // 'plane', 'circle', 'ring', // 2D shapes
+      'cone', 'cylinder', 'sphere', 'box', // 3D shapes
+      'tetrahedron', 'octahedron', 'dodecahedron', 'icosahedron', // Polyhedra
+      'torus', 'knot', 'random knot', 'lathe', // Irregular shapes
+    ];
+    return randomValue(shapes);
+  }
+
   initGeometry(args) {
     args = args || {};
-    // Create sphere geometry
-    this.geometry = new THREE.SphereBufferGeometry(this.size, 20, 16);
+    // Set up geometry properties
+    const size = this.size;
+    const radius = this.size;
+    const height = this.size * Math.sqrt(2);
+    // Create geometry based on shape name
+    const shape = args.shape || Shape.randomShape();
+    // 2D shapes
+    if (shape === 'plane' || shape === 'square' || shape === 'rectangle')
+      this.geometry = new THREE.PlaneBufferGeometry(size * 1.8, size * 1.8);
+    else if (shape === 'circle' || shape === 'disk')
+      this.geometry = new THREE.CircleBufferGeometry(radius, 24);
+    else if (shape === 'ring' || shape === 'record')
+      this.geometry = new THREE.RingBufferGeometry(radius / 2, radius, 24);
+    // 3D circular shapes
+    else if (shape === 'cone' || shape === 'ice cream')
+      this.geometry = new THREE.ConeBufferGeometry(radius, height, 24);
+    else if (shape === 'cylinder' || shape === 'tube')
+      this.geometry = new THREE.CylinderBufferGeometry(radius, radius, height, 24);
+    else if (shape === 'sphere' || shape === 'ball')
+      this.geometry = new THREE.SphereBufferGeometry(radius, 20, 16);
+    // 3D regular polyhedra (Platonic solids)
+    else if (shape === 'box' || shape === 'cube' || shape === 'd6')
+      this.geometry = new THREE.BoxBufferGeometry(height, height, height);
+    else if (shape === 'tetrahedron' || shape === 'pyramid' || shape === 'd4')
+      this.geometry = new THREE.TetrahedronBufferGeometry(size * 1.5);
+    else if (shape === 'octahedron' || shape === 'd8')
+      this.geometry = new THREE.OctahedronBufferGeometry(size * 1.3);
+    else if (shape === 'dodecahedron' || shape === 'd12')
+      this.geometry = new THREE.DodecahedronBufferGeometry(size * 1.2);
+    else if (shape === 'icosahedron' || shape === 'd20')
+      this.geometry = new THREE.IcosahedronBufferGeometry(size * 1.2);
+    // 3D irregular shapes
+    else if (shape === 'torus' || shape === 'donut')
+      this.geometry = new THREE.TorusBufferGeometry(radius * 3/4, radius / 4, 12, 24);
+    else if (shape === 'knot' || shape === 'pretzel')
+      this.geometry = new THREE.TorusKnotBufferGeometry(radius * 2/3, radius / 6, 64, 12);
+    else if (shape === 'random knot' || shape === 'random pretzel') {
+      // See https://threejs.org/docs/#api/en/geometries/TorusKnotBufferGeometry
+      const p = randomInt(1, 8); // How many times to wind around axis of rotational symmetry
+      const q = randomInt(1, 8); // How many times to wind around circle in interior of torus
+      this.geometry = new THREE.TorusKnotBufferGeometry(radius * 2/3, radius / 8, 128, 12, p, q);
+    }
+    // 3D parametric rotational shapes
+    else if (shape === 'lathe' || shape === 'vase') {
+      const numPoints = 64;
+      const points = [];
+      for (let i = 0; i < numPoints; i++) {
+        const x = size * 3/4 + size/4 * Math.sin(i * 0.18) * Math.sin(i * 0.1);
+        const y = (i - numPoints/2) * 1.4;
+        points.push(new THREE.Vector2(x, y));
+      }
+      this.geometry = new THREE.LatheBufferGeometry(points, 24);
+    }
+  }
+
+  initMesh(args) {
+    args = args || {};
     // Generate 2 random colors
     const colors = randomColor({
       count: 2,
@@ -25,9 +90,10 @@ class Shape {
       luminosity: 'dark',
     });
     // Create material and mesh
-    this.material = new THREE.MeshLambertMaterial({
+    this.material = new THREE.MeshPhongMaterial({
       color: args.color || colors[0],
       emissive: colors[1],
+      side: THREE.DoubleSide,
     });
     this.mesh = new THREE.Mesh(this.geometry, this.material);
     // Shape mesh coordinate properties
